0db11b5063710f82a10f74f590c3e01ebb11afa7
Alejaldo <statfluct@yahoo.com>
2020-12-28 22:24:49 +0500

    Itaration 1

---
+++ lib/event_manager1.rb
@@ -0,0 +1,15 @@
+require "csv"
+
+puts "EventManager Initialized!"
+puts
+
+if File.exist? "event_attendees.csv"
+
+  contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    puts "#{name} #{zipcode}"
+  end
+
+end
