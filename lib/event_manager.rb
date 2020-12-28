5eaa2866e29d4081400f32db0c8c03ed96602b42
Alejaldo <statfluct@yahoo.com>
2020-12-28 11:42:24 +0500

    Iteration 0

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,14 @@
-puts "EventManager Initialized!
+puts "EventManager Initialized!"
+puts
+
+if File.exist? "event_attendees.csv"
+
+  lines = File.readlines "event_attendees.csv"
+  lines.each_with_index do |line,index|
+    next if index == 0
+    columns = line.split(",")
+    name = columns[2]
+    puts name
+  end
+
+end
