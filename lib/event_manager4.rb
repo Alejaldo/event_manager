624c07dd0e9797a2fb6ea1b37b9249d45e1783a4
Alejaldo <statfluct@yahoo.com>
2020-12-30 18:52:05 +0500

    Adding clean_phone method

--- lib/event_manager4.rb
+++ lib/event_manager4.rb
@@ -11,7 +11,7 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address( #https://github.com/googleapis/google-api-ruby-client/blob/master/google-api-client/generated/google/apis/civicinfo_v2/service.rb
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
@@ -19,6 +19,19 @@ def legislators_by_zipcode(zip)
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
+end
+
+def clean_phone(phone)
+  clean_number = phone.gsub(/[^\d]/,"")
+  if clean_number.size < 10 || clean_number.size > 11
+    "Error. The number must have 10 digits or 11 digits if 1 is the first digit"
+  elsif clean_number.size == 11 && clean_number[0] == "1"
+    clean_number[1..10]
+  elsif clean_number.size == 11 && clean_number[0] != "1"
+    "Error. Unknown format"
+  else
+    clean_number
+  end 
 end
 
 def save_thank_you_letter(id,form_letter)
